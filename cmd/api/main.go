8a547a2b7dabd02046ddc6d47135224de22980bf
Fady Philip <fadyph2003@gmail.com>
2026-06-29 15:56:36 +0300

    feat(handler): complete HTTP transport layer with mock service and dependency injection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"The-Vault/internal/domain"
+	"The-Vault/internal/handler"
+	"context"
+	"net/http"
+	"time"
+)
+
+type MockSecretService struct {
+}
+
+func (m *MockSecretService) CreateSecret(ctx context.Context, name string, plaintext string) (*domain.Secret, error) {
+	return &domain.Secret{
+		ID:         "isub",
+		Name:       "anytihing",
+		Ciphertext: []byte{},
+		CreatedAt:  time.Now(),
+		ExpiresAt:  time.Now(),
+		Nonce:      []byte{},
+	}, nil
+
+}
+
+func main() {
+	mockservice := MockSecretService{}
+
+	myhandler := handler.NewSecretHandler(&mockservice)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /v1/secrets", myhandler.CreateSecret)
+	http.ListenAndServe(":8080", mux)
+
+}
